a796fb9fd65a092b272b0410280bd08457cc0c9d
Raherinotoavina <raherinotoavinasa@gmail.com>
2024-04-23 20:36:30 +0300

    introduction to struct data type

---
+++ CHAP-4/a_defining_instantiating_structs/target/debug/deps/a_defining_instantiating_structs-5b58bc21992b2f60.d
@@ -0,0 +1,5 @@
+/home/safidy/Public/Fianarana/Programing_Langage/Rust/Documentation/CHAP-4/a_defining_instantiating_structs/target/debug/deps/liba_defining_instantiating_structs-5b58bc21992b2f60.rmeta: src/main.rs
+
+/home/safidy/Public/Fianarana/Programing_Langage/Rust/Documentation/CHAP-4/a_defining_instantiating_structs/target/debug/deps/a_defining_instantiating_structs-5b58bc21992b2f60.d: src/main.rs
+
+src/main.rs:
